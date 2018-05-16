5aca0dc772d368515a20d27f33a729f5a8fa6326
Chad Song <sackcentury@gmail.com>
2018-05-16 22:45:07 +0800

    Update api.py

--- neteaseApi/api.py
+++ neteaseApi/api.py
@@ -628,7 +628,7 @@ class NetEase(object):
         temp = []
         if dig_type == 'songs' or dig_type == 'fmsongs':
             for i in range(0, len(data)):
-                url, quality, play_time = geturl(data[i])
+                url, quality, play_time = geturl_new_api(data[i])
                 if data[i]['album'] is not None:
                     album_name = data[i]['album']['name']
                     album_id = data[i]['album']['id']
@@ -689,7 +689,7 @@ class NetEase(object):
                 temp.append(playlists_info)
 
         elif dig_type == 'channels':
-            url, quality = geturl(data)
+            url, quality = geturl_new_api(data)
             channel_info = {
                 'song_id': data['id'],
                 'song_name': data['name'],
